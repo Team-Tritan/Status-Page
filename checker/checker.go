057441a8b5ffaa53f1abfde0dd08ae6b65a86198
Dylan <dylan@tritan.dev>
2023-08-25 02:52:58 -0400

    HOLY FUCK FINALLY

--- checker/checker.go
+++ checker/checker.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"sync"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"tritan.dev/status-page/config"
 )
@@ -23,9 +25,9 @@ type Check struct {
 }
 
 func Init(cfg config.Config, client *mongo.Client) {
-	for {
-		var checks []*Check 
+	var mu sync.Mutex
 
+	for {
 		for _, service := range cfg.Services.Services {
 			address := fmt.Sprintf("%s:%s", service.Hostname, service.Port)
 			start := time.Now()
@@ -37,49 +39,59 @@ func Init(cfg config.Config, client *mongo.Client) {
 				status = "FAIL"
 			}
 
-			var check *Check
-			for i := range checks {
-				if checks[i].Title == service.Title {
-					check = checks[i]
-					break
+			check := &Check{
+				Title:       service.Title,
+				Hostname:    service.Hostname,
+				Port:        service.Port,
+				Description: service.Description,
+			}
+
+			mu.Lock()
+
+			collection := client.Database("status-page").Collection("services")
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			var existingService Check
+			err = collection.FindOne(ctx, bson.M{"title": check.Title}).Decode(&existingService)
+
+			if err == nil {
+				existingService.Statuses = append(existingService.Statuses, struct {
+					Status  string
+					Latency string
+					Date    time.Time
+				}{
+					Status:  status,
+					Latency: fmt.Sprintf("%dms", elapsed),
+					Date:    time.Now(),
+				})
+
+				_, err := collection.UpdateOne(
+					ctx,
+					bson.M{"title": existingService.Title},
+					bson.M{"$set": bson.M{"statuses": existingService.Statuses}},
+				)
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				check.Statuses = []struct {
+					Status  string
+					Latency string
+					Date    time.Time
+				}{{
+					Status:  status,
+					Latency: fmt.Sprintf("%dms", elapsed),
+					Date:    time.Now(),
+				}}
+
+				_, err := collection.InsertOne(ctx, check)
+				if err != nil {
+					fmt.Println(err)
 				}
 			}
 
-			if check == nil {
-				check = &Check{
-					Title:       service.Title,
-					Hostname:    service.Hostname,
-					Port:        service.Port,
-					Description: service.Description,
-				}
-				checks = append(checks, check)
-			}
-
-			check.Statuses = append(check.Statuses, struct {
-				Status  string
-				Latency string
-				Date    time.Time
-			}{
-				Status:  status,
-				Latency: fmt.Sprintf("%dms", elapsed),
-				Date:    time.Now(),
-			})
-		}
-
-		collection := client.Database("status-page").Collection("services")
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		if len(checks) > 0 {
-			documents := make([]interface{}, len(checks))
-			for i, check := range checks {
-				documents[i] = check
-			}
-
-			_, err := collection.InsertMany(ctx, documents)
-			if err != nil {
-				fmt.Println(err)
-			}
+			mu.Unlock()
 		}
 
 		time.Sleep(1 * time.Minute)
